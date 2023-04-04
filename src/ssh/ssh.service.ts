d6afe937d7b471088053afeed8e1f1a36a3dac08
Владислав Жигульский <upachko@gmail.com>
2023-04-04 23:47:00 +0300

    add auto scheduler for all servers in start app

--- src/ssh/ssh.service.ts
+++ src/ssh/ssh.service.ts
@@ -1,9 +1,10 @@
 import {
   BadRequestException,
   Injectable,
+  Logger,
   NotFoundException,
 } from '@nestjs/common';
-import { DataSource, EntityManager, Repository } from 'typeorm';
+import { DataSource, EntityManager, IsNull, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Ssh } from './entities/ssh.entity';
 import CreateSshDto from './dto/create-ssh.dto';
@@ -17,8 +18,14 @@ import { FindManyOptions } from 'typeorm/find-options/FindManyOptions';
 import SshClientFactory from './client/SshClientFactory';
 import { Job } from '../jobs/entities/job.entity';
 import { LogService } from '../log/log.service';
+import { CronExpression, SchedulerRegistry, Timeout } from '@nestjs/schedule';
+import { CronJob } from 'cron';
+import Sentry from '@sentry/node';
 @Injectable()
 export class SshService {
+  private readonly logger = new Logger(SshService.name);
+  private serversInProgress = new Set();
+
   constructor(
     @InjectRepository(Ssh)
     private readonly sshRepository: Repository<Ssh>,
@@ -26,12 +33,56 @@ export class SshService {
     private readonly dataSource: DataSource,
     private readonly i18n: I18nService<I18nTranslations>,
     private readonly logService: LogService,
+    private readonly schedulerRegistry: SchedulerRegistry,
   ) {}
   async getMany(manager?: EntityManager): Promise<Ssh[]> {
     return this.__filter({}, manager);
   }
 
-  async upsertLogs(id: number) {
+  @Timeout(1000)
+  private async setAllServersInSchedule() {
+    this.logger.log(this.i18n.t('ssh.messages.start_all_servers'));
+    const serverIds = await this.__filter({
+      select: ['id'],
+      where: { deleted_at: IsNull() },
+    }).then((res) => res.map((el) => el.id));
+    this.logger.log(
+      this.i18n.t('ssh.messages.need_add_count', {
+        args: { cnt: serverIds.length },
+      }),
+    );
+    for (let i = 0, c = serverIds.length; i < c; i++) {
+      const id = serverIds[i];
+      const job = new CronJob(
+        CronExpression.EVERY_MINUTE,
+        this.serverLogCronJobWrapper(id),
+      );
+      this.schedulerRegistry.addCronJob(`cronjob_logs_for_server_${id}`, job);
+      job.start();
+      this.logger.log(
+        this.i18n.t('ssh.messages.add_server_id', { args: { id } }),
+      );
+    }
+  }
+
+  private serverLogCronJobWrapper(id) {
+    return async () => {
+      if (this.serversInProgress.has(id)) return;
+      this.logger.debug(
+        this.i18n.t('ssh.messages.cron_job_start', { args: { id } }),
+      );
+      this.serversInProgress.add(id);
+      await this.upsertLogs(id).catch((err) => {
+        this.logger.error(err);
+        Sentry.captureException(err);
+      });
+      this.logger.debug(
+        this.i18n.t('ssh.messages.cron_job_end', { args: { id } }),
+      );
+      this.serversInProgress.delete(id);
+    };
+  }
+  private async upsertLogs(id: number) {
     const sshEntity = await this.getById(id);
     const client = await SshClientFactory.getSSHInstance({
       host: sshEntity.host,

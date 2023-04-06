6b07735cd803363b9de9e130acbb39aa8ec44a6f
Владислав Жигульский <upachko@gmail.com>
2023-04-06 12:11:07 +0300

    upd

---
+++ tests/jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  rootDir: '../',
+  testRegex: '.*\\.spec\\.ts$',
+  testEnvironment: 'node',
+  verbose: true,
+  bail: 5,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!**/helpers/**',
+    '!**/migrations/**',
+    '!**/config/**',
+    '!src/main.ts',
+  ],
+  coverageDirectory: 'coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
+
+export default config;

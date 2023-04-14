ea6a5238fa432170bdb5789026e968c53478d6f9
Zhigulskiy Vladislav <upachko@gmail.com>
2023-04-14 14:19:39 +0300

    no message

--- src/migrations/1681460599023-new-generated.ts
+++ src/migrations/1681460599023-new-generated.ts
@@ -1,18 +1,29 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class newGenerated1681460599023 implements MigrationInterface {
-    name = 'newGenerated1681460599023'
+  name = 'newGenerated1681460599023';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`ssh\` DROP FOREIGN KEY \`FK_840c9422c9dbac17524dcbccd33\``);
-        await queryRunner.query(`ALTER TABLE \`ssh\` CHANGE \`userEntityId\` \`orgEntityId\` int NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`ssh\` ADD CONSTRAINT \`FK_505251dad5c1a1c63521b91c2a8\` FOREIGN KEY (\`orgEntityId\`) REFERENCES \`organization\`(\`id\`) ON DELETE RESTRICT ON UPDATE CASCADE`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`ssh\` DROP FOREIGN KEY \`FK_840c9422c9dbac17524dcbccd33\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`ssh\` CHANGE \`userEntityId\` \`orgEntityId\` int NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`ssh\` ADD CONSTRAINT \`FK_505251dad5c1a1c63521b91c2a8\` FOREIGN KEY (\`orgEntityId\`) REFERENCES \`organization\`(\`id\`) ON DELETE RESTRICT ON UPDATE CASCADE`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`ssh\` DROP FOREIGN KEY \`FK_505251dad5c1a1c63521b91c2a8\``);
-        await queryRunner.query(`ALTER TABLE \`ssh\` CHANGE \`orgEntityId\` \`userEntityId\` int NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`ssh\` ADD CONSTRAINT \`FK_840c9422c9dbac17524dcbccd33\` FOREIGN KEY (\`userEntityId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`ssh\` DROP FOREIGN KEY \`FK_505251dad5c1a1c63521b91c2a8\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`ssh\` CHANGE \`orgEntityId\` \`userEntityId\` int NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`ssh\` ADD CONSTRAINT \`FK_840c9422c9dbac17524dcbccd33\` FOREIGN KEY (\`userEntityId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 }

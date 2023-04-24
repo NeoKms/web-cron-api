f257f273da623ccfd3301228850309aeca7237e8
Владислав Жигульский <upachko@gmail.com>
2023-04-24 23:29:03 +0300

    upd

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -1,12 +1,5 @@
 import { Exclude, Expose } from 'class-transformer';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 @Exclude()
 export class ChangePasswordDto {
